6af935dc1468ddd9837a11684705bcf2deaddad0
Diogo <agenciavetordigital@gmail.com>
2025-09-17 15:41:09 -0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// Configuração do Supabase
-const { createClient } = supabase;
-
-const SUPABASE_URL = "https://dekubpsswkfrtotwljvf.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRla3VicHNzd2tmcnRvdHdsanZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNzYyOTAsImV4cCI6MjA3MzY1MjI5MH0.Winuq2LIkaGt1sGNXUhi__Q5_AczyxKAYQ_ORABoPTk"; // configure policies no Supabase!
-
-export const db = createClient(SUPABASE_URL, SUPABASE_KEY);
